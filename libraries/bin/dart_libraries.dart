976793a86b4922b3838abf0702e1022962cb99fd
Venkatpublic <venkatasalam0412@gmail.com>
2025-11-14 12:09:25 +0530

    async generator groundups

--- libraries/bin/dart_libraries.dart
+++ libraries/bin/dart_libraries.dart
@@ -3,32 +3,17 @@ import 'dart:ffi';
 import 'dart:isolate';
 import 'dart:io';
 
-void main() async {
-  // Stream.periodic(
-  //   const Duration(seconds: 1),
-  //   (x) => x,
-  // ).listen((item) => print(item));
-  // Stream.fromFutures([Future(() => 3), Future.value(33)]).listen(print);
+void main() {
+  asyncGenerator().listen((param) => print("Data $param"));
+}
 
-  final streamController = StreamController.broadcast();
-  final subScriber = streamController.stream.listen(print);
-  final subScriber2 = streamController.stream.listen((param) {
-    print("second $param");
-  });
-  var val = 0;
-  Timer.periodic(const Duration(seconds: 1), (timer) {
-    if (val == 5) {
-      timer.cancel();
-      streamController.close();
-      subScriber.cancel();
-      subScriber2.cancel();
-    } else {
-      streamController.add(val++);
-    }
-  });
-  var max = 0;
-  await for (final value in streamController.stream) {
-    max = (value > max) ? value : max;
+Stream<int> asyncGeneratorNega(param) async* {
+  yield -param;
+}
+
+Stream<int> asyncGenerator() async* {
+  for (int i = 0; i <= 5; i++) {
+    yield i;
+    yield* asyncGeneratorNega(i);
   }
-  print("max is $max");
 }
