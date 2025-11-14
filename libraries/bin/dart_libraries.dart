9f4012c362eaaa8583d8005e863efc3c570c7cdd
Venkatpublic <venkatasalam0412@gmail.com>
2025-11-14 15:59:01 +0530

    Practice

--- libraries/bin/dart_libraries.dart
+++ libraries/bin/dart_libraries.dart
@@ -3,11 +3,46 @@ import 'dart:ffi';
 import 'dart:isolate';
 import 'dart:io';
 
+Map<String, int> studentMarkList = {
+  "Aarav": 67,
+  "Vihaan": 54,
+  "Ishaan": 78,
+  "Rohan": 52,
+  "Arjun": 29,
+  "Kartik": 76,
+  "Dev": 35,
+  "Sanjay": 87,
+  "Pranav": 45,
+  "Ritesh": 32,
+  "Aditya": 90,
+  "Harsh": 90,
+  "Raghav": 85,
+};
 void main(List<String> args) {
-  var func = callableClass();
-  func(10000);
+  int firstMark = studentMarkList.values.toList().reduce(whichIsBigger);
+  List<String> failedList = List.empty(growable: true);
+  List<String> toppersList = List.empty(growable: true);
+  studentMarkList.forEach(
+    (name, mark) => (mark < 35) ? failedList.add(name) : null,
+  );
+  double passPercentage =
+      ((studentMarkList.length - failedList.length) / studentMarkList.length) *
+      100;
+  var topper = studentMarkList.entries
+      .where((element) => element.value == firstMark)
+      .toList();
+  for (int i = 0; i < topper.length; i++) {
+    toppersList.add(topper[i].key);
+  }
+  print(
+    "First mark:$firstMark,pass percentage:$passPercentage,toppers:${toppersList.join(',')}",
+  );
 }
 
-class callableClass {
-  void call(dynamic param) => print("User have given $param as input");
+int whichIsBigger(int prev, int curr) {
+  if (prev > curr) {
+    return prev;
+  } else {
+    return curr;
+  }
 }
