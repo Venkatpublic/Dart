3bb6a1e0e11597c8def70c69fc87d1b352602f55
Venkatpublic <venkatasalam0412@gmail.com>
2025-11-14 16:22:01 +0530

    closure

--- libraries/bin/dart_libraries.dart
+++ libraries/bin/dart_libraries.dart
@@ -1,48 +1,12 @@
-import 'dart:async';
-import 'dart:ffi';
-import 'dart:isolate';
-import 'dart:io';
-
-Map<String, int> studentMarkList = {
-  "Aarav": 67,
-  "Vihaan": 54,
-  "Ishaan": 78,
-  "Rohan": 52,
-  "Arjun": 29,
-  "Kartik": 76,
-  "Dev": 35,
-  "Sanjay": 87,
-  "Pranav": 45,
-  "Ritesh": 32,
-  "Aditya": 90,
-  "Harsh": 90,
-  "Raghav": 85,
-};
-void main(List<String> args) {
-  int firstMark = studentMarkList.values.toList().reduce(whichIsBigger);
-  List<String> failedList = List.empty(growable: true);
-  List<String> toppersList = List.empty(growable: true);
-  studentMarkList.forEach(
-    (name, mark) => (mark < 35) ? failedList.add(name) : null,
-  );
-  double passPercentage =
-      ((studentMarkList.length - failedList.length) / studentMarkList.length) *
-      100;
-  var topper = studentMarkList.entries
-      .where((element) => element.value == firstMark)
-      .toList();
-  for (int i = 0; i < topper.length; i++) {
-    toppersList.add(topper[i].key);
-  }
-  print(
-    "First mark:$firstMark,pass percentage:$passPercentage,toppers:${toppersList.join(',')}",
-  );
+Function outerShell(int first) {
+  return (int second) => innerShell(first, second);
 }
 
-int whichIsBigger(int prev, int curr) {
-  if (prev > curr) {
-    return prev;
-  } else {
-    return curr;
-  }
+int innerShell(int first, int second) {
+  return first + second;
+}
+
+void main(List<String> args) {
+  Function one = outerShell(100);
+  print("${one(50)}${one(100)}");
 }
