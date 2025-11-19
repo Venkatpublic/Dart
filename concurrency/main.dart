1fba668cea16cc424a105b2fbe5609f1d4ea5699
Venkatpublic <venkatasalam0412@gmail.com>
2025-11-19 14:33:43 +0530

    concurrency learning 2

--- concurrency/main.dart
+++ concurrency/main.dart
@@ -5,11 +5,18 @@ import 'dart:io';
 const String filename = 'data.json';
 
 void main() {
-  print("Start");
+  Stream<int> stream = Stream.periodic(const Duration(seconds: 1), (i) {
+    print("emitting $i");
+    return i;
+  });
+  sumStream(stream).listen((j) => print("looped $j"));
+}
 
-  _readFileSync();
-  _readFileAsync();
-  print("End");
+Stream<int> sumStream(Stream<int> stream) async* {
+  var sum = 0;
+  await for (final value in stream) {
+    yield sum += value;
+  }
 }
 
 void _readFileSync() {
