a4b490fcd6c40ff10da1093e33ecad7c9a7804b4
Venkatpublic <venkatasalam0412@gmail.com>
2025-11-21 18:31:43 +0530

    Completer class learning

--- isolates/main.dart
+++ isolates/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:isolate';
 
 import 'resilient_worker.dart';
@@ -25,12 +26,45 @@ Future<String> returnmyName() async {
 // final worker = Worker();
 // await worker.spawn();
 // await worker.parseJson('{"key":"value"}');
+
+//Part 3
+// var worker = await ResilientWorker.spawn();
+// var data = await worker.parseJson('{"key":"value"}');
+// var data2 = await worker.parseJson(
+//     '{"key":"value","key2":"value","key3":"value","key4":"value","key5":"value","key6":"value","key7":"value","key":"value"}');
+// print("$data");
+// print("$data2");
+// worker.close();
+
+Future<String> completeFunction(state) {
+  final complete = Completer<String>();
+  Future<String> data = complete.future;
+  Future.delayed(Duration(seconds: 3), () {
+    if (state == true) {
+      complete.complete("Hello");
+    } else {
+      complete.completeError(Exception("Failure case"));
+    }
+  });
+  return data;
+}
+
 void main() async {
-  var worker = await ResilientWorker.spawn();
-  var data = await worker.parseJson('{"key":"value"}');
-  var data2 = await worker.parseJson(
-      '{"key":"value","key2":"value","key3":"value","key4":"value","key5":"value","key6":"value","key7":"value","key":"value"}');
-  print("$data");
-  print("$data2");
-  worker.close();
+  final completer = Completer<String>();
+  Future<String> future = completer.future;
+  Future.delayed(Duration(seconds: 2), () => completer.complete("Whassup"));
+  print("Waiting...");
+  String val = await future;
+  print(val);
+  try {
+    print(await completeFunction(true));
+  } catch (e) {
+    print(e);
+  }
+  try {
+    print(await completeFunction(false));
+  } catch (e) {
+    print("ERROR:$e");
+  }
+  ;
 }
